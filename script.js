c0ac33c219dbcc58e21dd9a3c41cb31942c8ce57
Jzu1991 <91jeas@gmail.com>
2026-05-07 13:57:41 -0300

    disminuir velocidad del cometa V2

--- script.js
+++ script.js
@@ -185,7 +185,7 @@ function fireComet() {
   if (inFlight || state !== 'playing' || paused) return;
   if (comets <= 0) return;
   const angle = getAimAngle();
-  const spd = 7;
+  const spd = 5;
   comet = {
     x: SAT_X, y: SAT_Y + 20,
     vx: Math.cos(angle) * spd,
